a36c7fac27b0d4be9e2dece8e7ac268246ba62f8
marc <mdugger@gmail.com>
2017-03-06 06:08:20 -0600

    extend Serializable per GAE's requirements

--- src/main/java/com/socotech/wf4j/WizardForm.java
+++ src/main/java/com/socotech/wf4j/WizardForm.java
@@ -1,12 +1,14 @@
 package com.socotech.wf4j;
 
+import java.io.Serializable;
+
 /**
  * Created with IntelliJ IDEA.
  * User: marc
  * Date: 2/25/17
  * Time: 6:03 PM
  */
-public interface WizardForm {
+public interface WizardForm extends Serializable {
     int getPage();
 
     String getChoice();
